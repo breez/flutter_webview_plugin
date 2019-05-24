c10e9e0272170f6d72de572018d91c07bccccfc0
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2019-05-24 05:57:20 +0300

    Add scripts for including and initializing webln, searching for ln links across DOM

--- android/src/main/java/com/flutter_webview_plugin/BrowserClient.java
+++ android/src/main/java/com/flutter_webview_plugin/BrowserClient.java
@@ -37,12 +37,62 @@ public class BrowserClient extends WebViewClient {
         data.put("url", url);
 
         FlutterWebviewPlugin.channel.invokeMethod("onUrlChanged", data);
-        String script = "if (!window.breezReceiveMessage) {" +
-                    "window.breezReceiveMessage = function(event) {Android.getPostMessage(event.data);};" +
-                    "window.addEventListener('message', window.breezReceiveMessage, false);" +
+        String listenPostMessagesScript =
+                "if (!window.breezReceiveMessage) {" +
+                "   window.breezReceiveMessage = function(event) {Android.getPostMessage(event.data);};" +
+                "   window.addEventListener('message', window.breezReceiveMessage, false);" +
                 "}";
-        view.evaluateJavascript(script, null);
+        view.evaluateJavascript(listenPostMessagesScript, null);
 
+        String includeWebLN =
+                        "var webLnLib = document.createElement(\"script\");\n" +
+                        "webLnLib.type = \"text/javascript\";\n" +
+                        "webLnLib.src = \"https://unpkg.com/webln@0.2.0/dist/webln.min.js\";\n" +
+                        "document.body.appendChild(webLnLib);";
+        view.evaluateJavascript(includeWebLN, null);
+
+        String initWebLN =
+                        "var webln = {\n" +
+                        "   enable: function () {\n" +
+                        "       window.postMessage(JSON.stringify({ enable: true }));\n" +
+                        "       return new Promise(function (resolve, reject) {resolve(true);});\n" +
+                        "   },\n" +
+                        "   sendPayment: function (paymentRequest) {\n" +
+                        "       window.postMessage(JSON.stringify({ uri: paymentRequest }));\n" +
+                        "       return new Promise(function (resolve, reject) { });\n" +
+                        "   },\n" +
+                        "};\n" +
+                        "setTimeout(function () { WebLN.requestProvider();},450);";
+        view.evaluateJavascript(initWebLN, null);
+
+        String listenLNLinks =
+                        "setInterval(function () {\n" +
+                        "   var searchText = \"lnbc\";\n" +
+                        "   var aTags = document.getElementsByTagName(\"span\");\n" +
+                        "   var i;\n" +
+                        "   for (i = 0; i < aTags.length; i++) {\n" +
+                        "       if (aTags[i].textContent.indexOf(searchText) === 0) {\n" +
+                        "           webln.sendPayment(aTags[i].textContent);\n" +
+                        "       }\n" +
+                        "   }\n" +
+                        "   /* ------------------------- */\n" +
+                        "   aTags = document.getElementsByTagName(\"input\");\n" +
+                        "   for (i = 0; i < aTags.length; i++) {\n" +
+                        "       if (aTags[i].value.indexOf(searchText) === 0) {\n" +
+                        "           webln.sendPayment(aTags[i].value);\n" +
+                        "       }\n" +
+                        "   }\n" +
+                        "   /* ------------------------- */\n" +
+                        "   aTags = document.getElementsByTagName(\"a\");\n" +
+                        "   searchText = \"lightning:lnbc\";\n" +
+                        "   for (i = 0; i < aTags.length; i++) {\n" +
+                        "       var href = aTags[i].getAttribute('href') + '';\n" +
+                        "       if (href.indexOf(searchText) === 0) {\n" +
+                        "           webln.sendPayment(href.replace('lightning:', ''));\n" +
+                        "       }\n" +
+                        "   }\n" +
+                        "}, 500);";
+        view.evaluateJavascript(listenLNLinks, null);
 
         data.put("type", "finishLoad");
         FlutterWebviewPlugin.channel.invokeMethod("onState", data);
